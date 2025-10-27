ebeaf786938689234523d4acd126f694f79de58b
Manidheep1234 <192425089.simats@saveetha.com>
2025-10-27 13:47:44 +0530

    Add files via upload

---
+++ py 8.py
@@ -0,0 +1,19 @@
+knowledge_base = {
+    "mammal(A)": ["vertebrate(A)"],
+    "vertebrate(A)": ["animal(A)"],
+    "vertebrate(A),flying(A)": ["bird(A)"],
+    "vertebrate('duck')": [],
+    "flying('duck')": [],
+    "mammal('cat')": []
+}
+
+def backward_chaining(goal):
+    for rule, conclusions in knowledge_base.items():
+        for conclusion in conclusions:
+            if goal == conclusion:
+                print(f"{goal} is derived from {rule}")
+                return True
+    print(f"{goal} cannot be derived from the knowledge base")
+    return False
+
+backward_chaining("animal(A)")
